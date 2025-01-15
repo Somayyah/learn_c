1f980b97f4d5baf932a2ead6e028ec872fbe4f57
watari <watari@techwebunraveled.xyz>
2025-01-15 23:46:00 +0300

    using define

--- Cel-Feh.c
+++ Cel-Feh.c
@@ -9,15 +9,13 @@ Section 1.2
 for fahr = 0, 20, ..., 300 
 */
 
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
+
 void main()
 {
-    // Define variables for Fehrinheit, lower and upper
-    float f = 0;
-    float c = 0;
-    while(f <= 300)
-    {
-        c = (5.0/9.0) * (f-32.0) ;
-        printf("%3.0f %6.1f\n", f, c);
-        f = f + 20;
-    }
+    for (float fahr = LOWER ; fahr <= UPPER ; fahr = fahr + STEP)
+        printf("%3.1f %6.1f\n", fahr , (5.0/9.0) * (fahr-32) );
+
 }

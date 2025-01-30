2d49b8bb9e524148f55dcc48b8116dcfffbbd431
watari <watari@techwebunraveled.xyz>
2025-01-30 15:07:22 +0300

    arrays updated

--- arrays.c
+++ arrays.c
@@ -5,36 +5,26 @@
 * several aspects of C in one program.                              *
  *******************************************************************/ 
 
-// Forgot to count the digits  
+// Trying the book's solution 
 
 #include <stdio.h>
 
 int main()
 {   
-    int c, cn, bn, nn, tn ; // Characters, Blanks, Newlines, Tabs
+    int c, cn, bn ; // Characters, Blanks, Newlines, Tabs
     int ndigits[10];        // Number of occurrences of each digit
 
     for (int i = 0; i < 10; i++) {
         ndigits[i] = 0;
     } 
-    cn = bn = nn = tn = 0;
+    cn = bn = 0;
     while ((c = getchar()) != EOF)
     {
-        if (c == ' ') 
+        if (c == ' ' || c == '\n' || c == '\t') 
             { 
                 ++bn;
                 continue;
             }
-        if (c == '\n')
-            {
-                ++nn;
-                continue;
-            }
-        if (c == '\t')
-        {
-            ++tn;
-            continue;
-        }
         if (c >= '0' && c <= '9')
         {
             ++ndigits[c - '0']; // Increment the count for the specific digit
@@ -46,8 +36,6 @@ int main()
     // Print counts
     printf("Characters: %d\n", cn);
     printf("Blanks: %d\n", bn);
-    printf("Newlines: %d\n", nn);
-    printf("Tabs: %d\n", tn);
     
     printf("Digits count:\n");
     for (int i = 0; i < 10; i++) {

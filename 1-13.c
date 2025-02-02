e2e1eb1d0f528e1c6eb36a753cb2689e4153bd80
watari <watari@techwebunraveled.xyz>
2025-02-02 20:53:18 +0300

    1-13 exercise

--- 1-13.c
+++ 1-13.c
@@ -6,6 +6,9 @@
 
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 int main()
 {
     int c, histogram[10];
@@ -16,7 +19,9 @@ int main()
     }
     
     while ((c = getchar()) != EOF)
-    ;
+    {
+        
+    }
 
     return 0;
 }

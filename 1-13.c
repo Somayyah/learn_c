9aa00a19d8256fd263532a198c76df60009cc5ee
watari <watari@techwebunraveled.xyz>
2025-02-02 20:30:36 +0300

    1-13 exercise

---
+++ 1-13.c
@@ -0,0 +1,23 @@
+/********************************************************************
+* Exercise 1-13. Write a program to print a histogram of the lengths*
+* of words in its input. It is easy to draw the histogram with the  *
+* bars horizontal; a vertical orientation is more challenging.      *
+*********************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int c, histogram[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        histogram[i] = 0;
+    }
+    
+    while ((c = getchar()) != EOF)
+    ;
+
+    return 0;
+}
+

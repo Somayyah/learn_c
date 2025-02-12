67ffc7b0c6c222dd56f90fb83a7b83802edc09fe
watari <watari@techwebunraveled.xyz>
2025-02-12 13:17:41 +0300

    1-14

--- 1-14_horizontal.c
+++ 1-14_horizontal.c
@@ -4,26 +4,29 @@
 */
 #include <stdio.h>
 
-
 int main()
 {
     int c = 0 ; 
     int chars[95] = {0}; // array to hold ascii characters from ' ' to '~'
-
+    int isprinted = 0;
     while ((c = getchar()) != EOF)
     {
         if (c >= ' ' && c <= '~') ++chars[c-' '];
     }
 
-    printf("\nC : F\n");
+    printf("\nC  : F\n");
     for (int i = 32; i < 127; i++)
     {
         if ( chars[i - ' '] > 0)
         {
-            printf("%c : ", i);
+            if (i == 32) printf("SP : ");
+            else printf("%c  : ", i);
             for (int j = 0; j < chars[i-' ']; j++) putchar('*');
             putchar('\n');
+            isprinted = 1;
         }
     }
+
+    if (isprinted == 0) printf("No printable ASCII characters (' ' to '~') found in input.\n");;
     return 0;
 }

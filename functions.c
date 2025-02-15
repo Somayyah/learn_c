cda109e89ec20c68de192acc30b104dab61e7282
watari <watari@techwebunraveled.xyz>
2025-02-15 12:24:12 +0300

    functions

--- functions.c
+++ functions.c
@@ -6,21 +6,21 @@ int main()
 {
     double i;
     for (i = -5; i < 10; ++i)
-        printf("%0.2f %0.2lf %0.2lf\n", i, power(2,i), power(-3,i));
+        printf("%0.0f %0.9lf %0.9lf\n", i, power(2,i), power(-3,i));
     return 0;
 }
 
 double power(int i, int j)
 {
     if (j == 0) return 1;
-    long int res = i;
+    double res = 1;
     if (j < 0)
     {
-        for (j ; j < 1; j++)
+        for (j ; j < 0; j++)
             res = res * i;
-        return 1 / res;
+        return 1.0 / res;
     }
-    for (j ; j > 1 ; j--)
+    for (j ; j > 0 ; j--)
         res = res * i;
     return res;
 }

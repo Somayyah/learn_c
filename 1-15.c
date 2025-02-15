948a940c2f3553074469ed0499e5362d85356412
watari <watari@techwebunraveled.xyz>
2025-02-15 13:10:50 +0300

    1-15

---
+++ 1-15.c
@@ -0,0 +1,20 @@
+/*
+Exercise 1.15. Rewrite the temperature conversion 
+program of Section 1.2 to use a function for conversion.
+*/
+#include <stdio.h>
+
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
+
+double temp_in_cel(double fahr)
+{
+    return (5.0/9.0) * (fahr-32);
+}
+
+int main()
+{
+    for (float fahr = LOWER ; fahr <= UPPER ; fahr = fahr + STEP)
+        printf("%3.3f %6.3f\n", fahr , temp_in_cel(fahr));
+}

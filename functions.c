381ec86389f6761d9a62e01782b2a84a0f361330
watari <watari@techwebunraveled.xyz>
2025-02-15 11:51:02 +0300

    functions

--- functions.c
+++ functions.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
-long int power(int i, int j)
-{
-    long int res = i;
-    for (j ; j > 1 ; j--)
-        res = res * i;
-    return res;
-}
+double power(int i, int j);
 
 int main()
 {
-    printf("%ld\n", power(2,32));
+    double i;
+    for (i = -5; i < 10; ++i)
+        printf("%0.2f %0.2lf %0.2lf\n", i, power(2,i), power(-3,i));
     return 0;
+}
+
+double power(int i, int j)
+{
+    if (j == 0) return 1;
+    long int res = i;
+    if (j < 0)
+    {
+        for (j ; j < 1; j++)
+            res = res * i;
+        return 1 / res;
+    }
+    for (j ; j > 1 ; j--)
+        res = res * i;
+    return res;
 }

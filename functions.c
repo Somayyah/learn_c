8c1a171209343e11e5c8a69546e3f02f504d0bc2
watari <watari@techwebunraveled.xyz>
2025-02-15 12:38:51 +0300

    final function

--- functions.c
+++ functions.c
@@ -4,23 +4,24 @@ double power(int i, int j);
 
 int main()
 {
-    double i;
-    for (i = -5; i < 10; ++i)
-        printf("%0.0f %0.9lf %0.9lf\n", i, power(2,i), power(-3,i));
+
+    for (int i = -5; i < 10; ++i)
+        printf("%d %0.5lf %0.5lf\n", i, power(2,i), power(-3,i));
     return 0;
 }
 
 double power(int i, int j)
 {
-    if (j == 0) return 1;
-    double res = 1;
-    if (j < 0)
-    {
-        for (j ; j < 0; j++)
-            res = res * i;
-        return 1.0 / res;
-    }
-    for (j ; j > 0 ; j--)
+    if (i == 0 && j < 0) return 0; //  Undefined treated as 0
+    if (j == 0) return 1; // By convention
+
+    double res = 1.0;
+
+    int abs_exp = ( j > 0) ? j : -j;
+    
+    for ( ; abs_exp > 0 ; abs_exp--)
         res = res * i;
+
+    if (j < 0) return 1.0 / res;
     return res;
 }

a991f075982dfd73aa990ae6d23bfed640886da3
watari <watari@techwebunraveled.xyz>
2025-02-16 22:41:46 +0300

    char arrays

---
+++ c/char-array.c
@@ -0,0 +1,30 @@
+/*
+write a program to do this:
+
+while (there's another line)
+    if (it's longer than the previous longest)
+        (save it)
+        (save its length)
+print longest line
+*/
+
+void getline();
+void copy();
+
+#include <stdio.h>
+
+int main()
+{
+
+    return 0;
+}
+
+void getline()
+{
+
+}
+
+void copy()
+{
+    
+}

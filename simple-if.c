986195a908c9557c27b41788281a05ebf19af79e
watari <watari@techwebunraveled.xyz>
2025-02-16 18:05:30 +0300

    patching exercise

--- simple-if.c
+++ simple-if.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-        printf("wrong # of arguments!\n")
+        printf("wrong # of arguments!\n");
         return 1;
     }
     if (strcmp(argv[1], "Hello") == 0)

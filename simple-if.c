28689d96885c01596b562e1601c81f6091efd139
watari <watari@techwebunraveled.xyz>
2025-02-16 17:36:00 +0300

    patching exercise

---
+++ simple-if.c
@@ -0,0 +1,18 @@
+/*
+Modify the ELF file so the string is printed if the user enters wrong answer
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("wrong # of arguments!\n")
+        return 1;
+    }
+    if (strcmp(argv[1], "Hello") == 0)
+        printf("Hello!\n");
+    else printf("Try again!\n");
+    return 0;
+}

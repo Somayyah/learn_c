2cf20ba028b96b432416e120c53396be4870b683
watari <watari@techwebunraveled.xyz>
2025-02-19 22:38:52 +0300

    cal-farh.s

---
+++ asm/Cel-Feh.s
@@ -0,0 +1,43 @@
+# print Fahrenheit-Celsius table
+# for fahr 0 20 to 300 
+# cel =  (5.0/9.0) * (fahr-32) 
+
+.global _start
+.intel_syntax noprefix
+
+.section .text
+_start:
+    
+    # sys_write call to print "C    :   F"
+
+	mov     rax, 1
+	mov     rdi, 1	
+	lea     rsi, [header]
+	mov     rdx, 10
+	syscall
+
+    # Print a Fahr and Cel for 0
+    
+    mov     eax, 0          # fahr = 0
+    sub     eax, 32         # eax = fahr - 32 = 0 - 32 = -32
+    imul    eax, 5          # eax = (5.0) * eax
+    idiv    9             # eax = eax / 9
+    mov     [res], eax       # store eax in res
+
+    # sys_exit call to exit from the program
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+.section .data
+    header:
+        .asciz "C   :   F\n"
+    res: .byte 0  # Properly initialized memory
+    newline: .byte 10 # ascii value for a newline
+
+.section .bss
+
+# What to learn
+#      Loops
+#      Printing numbers and strings to stdout
+#      Arithmetics
---
+++ asm/temp.c
@@ -0,0 +1,6 @@
+int main()
+{
+    int a = 0;
+    int i = (5.0/9.0) * (a-32);
+    return i;
+}

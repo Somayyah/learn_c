e604b2a242fe1a856915b2132b6af9d5e121b95e
watari <watari@techwebunraveled.xyz>
2025-02-21 17:47:33 +0300

    conditions

--- asm/simple-loop.s
+++ asm/simple-loop.s
@@ -6,22 +6,28 @@
 .section .text
 _start:	
 
+	cmp counter, 4
+	jne print
+
+	# sys_exit call to exit from the program
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+print:
 	# sys_write call	
 	mov rax, 1
 	mov rdi, 1	
 	lea rsi, [hello_world]
 	mov rdx, 14 
 	syscall
-	
-	# sys_exit call to exit from the program
-	mov rax, 60
-	mov rdi, 0
-	syscall
+	add counter, 1
+	jmp	_start
 
-.loop:
-    
 .section .data
 hello_world:
 	.asciz "Hello, World!\n"
+counter:
+	db	0
 
-.section .bss 
+.section .bss
--- asm/temp.c
+++
@@ -1,6 +0,0 @@
-int main()
-{
-    int a = 0;
-    int i = (5.0/9.0) * (a-32);
-    return i;
-}

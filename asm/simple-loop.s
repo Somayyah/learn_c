657fd8ecbd266b3cbc73203232e129b0e05ad854
watari <watari@techwebunraveled.xyz>
2025-02-21 21:30:24 +0300

    print hello, world! 5 times

--- asm/simple-loop.s
+++ asm/simple-loop.s
@@ -1,33 +1,29 @@
-# Print "Hello, World!" 5 times
-
-.global _start
 .intel_syntax noprefix
+.global _start
 
 .section .text
 _start:	
 	jmp print
-	# sys_exit call to exit from the program
 
 exit_code:
+	# syscall to exit from the program
 	mov rax, 60
 	mov rdi, 0
 	syscall
 
 print:
-	# sys_write call	
+	# sys_write call to print "hello, world!"
 	mov rax, 1
 	mov rdi, 1	
 	lea rsi, [hello_world]
 	mov rdx, 14 
 	syscall
 	add byte ptr [counter], 1
-	cmp byte ptr [counter], 4
-	jle print
-	jmp exit_code
+	cmp byte ptr [counter], 4	# Did we print hello_world 5 times yet?
+	jle print					# If not print again
+	jmp exit_code				# Exit from the code
 
 .section .data
 hello_world:
 	.asciz "Hello, World!\n"
 counter: .byte 0
-
-.section .bss

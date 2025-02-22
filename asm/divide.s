0e85a05e09a7b86ece4cc99aaa92f313c4f32189
watari <watari@techwebunraveled.xyz>
2025-02-22 21:50:17 +0300

    printed 40

--- asm/divide.s
+++ asm/divide.s
@@ -5,14 +5,23 @@
 
 .section .text
 _start:
-
+    jmp     ex1
     # ex1 : 200 / 5 ( 0 - 255 ) 8 bit unsigned
+ex1:
     mov     ax, 200
     mov     bl, 5   
     div     bl
     jmp     store_number_on_stack
 
     # ex2 : -100 / 25 ( )
+ex2:
+    
+    # sys_write (write '\n' to stdout)
+    mov rdi, 1  # stdout
+    mov rax, 1  # sys_write syscall number
+    lea rsi, [newline]  # Address of newline
+    mov rdx, 1  # Write 1 byte
+    syscall
 
     # ex3 : 5000 / 100 ( )
 
@@ -25,7 +34,8 @@ _start:
     # ex7 : 100000000000 / 987654
 
     # ex8 : -9223372036854775808 / 4294967296
-
+    jmp     exit_code
+    
 exit_code:
     # sys_exit
     mov     rax, 60  # sys_exit syscall number
@@ -45,21 +55,23 @@ store_number_on_stack:
     jmp     store_number_on_stack 
 
 print_int:
-    pop     word [buffer]
+    pop     word ptr [buffer]
     mov     rdi, 1  # stdout
     mov     rax, 1  # sys_write syscall number
     lea     rsi, [buffer]  # Address of res
     mov     rdx, 2  # Write 2 byte
     syscall
+
     sub     byte ptr [iterator], 1
     cmp     byte ptr [iterator], 0
     jne     print_int
-    jmp     exit_code
+    jmp     ex2
 
 .section .data
 ex1_q:      .byte   0
 ex1_r:      .byte   0
 iterator:   .byte   0
-buffer:     .byte   2
+buffer:     .word   0
+newline: .byte 10 # ascii value for a newline
 
 .section .bss

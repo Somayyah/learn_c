264c5ede18779c34d59dc0b5b892f429b46a1df3
watari <watari@techwebunraveled.xyz>
2025-03-13 14:02:31 +0300

    printing numbers with sign

--- asm/print_number.s
+++ asm/print_number.s
@@ -3,8 +3,6 @@
 .intel_syntax noprefix
 
 .section .data
-buffer:		.byte   0
-sign:		.byte	0
 num1:   	.byte   42
 num2:   	.byte   -42
 num3:   	.word   12345
@@ -16,7 +14,7 @@ num8:   	.quad   -12345678901234
 iterator:   .byte   0		# How many digits to print
 buffer:     .word   0
 newline:    .byte   10      # ascii value for a newline
-sign:       .byte   43
+sign:       .byte   43   # for sign '+'
 
 .global _start
 
@@ -27,7 +25,7 @@ _start:
     xor     rdi, rdi
     syscall
 
-store_number_on_stack:   
+store_number_on_stack:
 	test  	eax, 0xFFFFFF00
 	jz		store_8_bit_on_stack
 	jmp		store_16_bit_on_stack
@@ -42,7 +40,8 @@ store_8_bit_on_stack:
     add     byte ptr [iterator], 1
     cmp     al, 0
     jne     store_8_bit_on_stack
-	jmp		after_storing
+	call	print_sign
+	jmp		print_int
 	ret
 
 store_16_bit_on_stack: 
@@ -54,22 +53,18 @@ store_16_bit_on_stack:
     add     byte ptr [iterator], 1
     cmp     eax, 0
     jne     store_16_bit_on_stack
-	jmp		after_storing
-	ret
+	call	print_sign
+	jmp		print_int
+	ret	
+
+print_sign:
+	mov     rdi, 1
+    mov     rax, 1
+    lea     rsi, [sign]  
+    mov     rdx, 2
+    syscall
+    ret	
 	
-after_storing:
-    cmp     byte ptr [sign], 45
-    je      push_sign
-    jmp     print_int
-    ret
-
-push_sign:
-    xor     cx, cx
-    mov     cl, byte ptr [sign] 
-    add     byte ptr [iterator], 1
-    push    cx
-    jmp     print_int
-
 print_int:
     pop     word ptr [buffer]
     mov     rdi, 1
@@ -80,24 +75,16 @@ print_int:
     sub     byte ptr [iterator], 1
     cmp     byte ptr [iterator], 0
     jne     print_int
-    ret
-
-cleanup:
-    mov     byte ptr [iterator], 0
-    mov     byte ptr [sign], 0
-    mov     word ptr [buffer], 0
+	
+print_newline:
     mov     rdi, 1
     mov     rax, 1
     lea     rsi, [newline]  
     mov     rdx, 1
     syscall
-	xor     rax, rax
-    xor     rbx, rbx
-    xor     rcx, rcx
-    ret
+	ret
 
 exit_code:
     mov     rax, 60
     mov     rdi, 0 
     syscall
-

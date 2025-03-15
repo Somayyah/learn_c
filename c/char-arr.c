378acf25ecd3c787b69740e17c955c2c46c7d7b4
watari <watari@techwebunraveled.xyz>
2025-03-15 14:00:38 +0300

    char-arrays

--- c/char-arr.c
+++ c/char-arr.c
@@ -4,18 +4,19 @@ int getline();
 
 int main()
 {
-	int longest_line[100] = "";
+	char longest_line[100] = "";
 	int c = 0;
 	int length = 0;
+	longest_line = getline();
 	
+	return 0;
+}
+
+char getline()
+{
 	while ((c = getchar()) != EOF )
 	{
 		
 	}
 	return 0;
-}
-
-int getline()
-{
-	
 }

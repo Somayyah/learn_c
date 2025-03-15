e5801d57d5ba56668d4fde156ae0c010cb029652
watari <watari@techwebunraveled.xyz>
2025-03-15 14:25:49 +0300

    char-arrays

--- c/char-arr.c
+++ c/char-arr.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int getline();
+int getline(int length, char longest_line[]);
 
 int main()
 {
 	char longest_line[100] = "";
 	int c = 0;
 	int length = 0;
-	longest_line = getline();
+	longest_line = getline(int length, char longest_line[]);
 	
 	return 0;
 }
 
-char getline()
+char getline(int length, char longest_line[])
 {
 	while ((c = getchar()) != EOF )
 	{

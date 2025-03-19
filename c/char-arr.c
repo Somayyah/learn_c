0818c86e28b34136ad5c93254527e9a02fdb35ca
watari <watari@techwebunraveled.xyz>
2025-03-19 12:31:35 +0300

    char-array done

--- c/char-arr.c
+++ c/char-arr.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int getline(char line[]);
-int copy();
+int lineLen(char line[]);
+int copy(char line[], char longest_line[], int len);
 
 #define MAXLength 1000
 
@@ -13,20 +13,35 @@ int main()
 	int MAXlen = 0;
 	while(fgets(line, MAXLength, stdin) != NULL)
 	{
-		length = getline(line);
+		length = lineLen(line);
 		if (length >= MAXlen)
 			MAXlen = length;
-		longest_line = line;
+		copy(line, longest_line, MAXlen);
 	}
+	printf("\nLongest Line: %s\nLength: %d\n", longest_line, MAXlen-1);
 	return 0;
 }
 
-int getline(char line[])
+int lineLen(char line[])
 {
-	
+	int counter = 0;
+	int len = 0;
+	while (line[counter] != '\0')
+	{
+		len++;
+		counter++;
+	}
+	return len;
+
 }
 
-int copy()
+int copy(char line[], char longest_line[], int len)
 {
-	
+	int counter = 0;
+	while(counter < len)
+	{
+		longest_line[counter] = line[counter];
+		counter++;
+	}
+	return 0;
 }

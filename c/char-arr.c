690d2143ea83abf4eea5b357f6259255f2245423
watari <watari@techwebunraveled.xyz>
2025-03-21 11:13:52 +0300

    longest line

--- c/char-arr.c
+++ c/char-arr.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int getline(char line[], int len);
+int updateline(char line[], int len);
 int lineLen(char line[]);
 int copy(char line[], char longest_line[]);
 
-#define MAXLength 1000
+#define MAXLength 100
 
 int main()
 {
@@ -12,13 +12,13 @@ int main()
 	char line[MAXLength] = "";
 	int length = 0;
 	int MAXlen = 0;
-	while((length = getline(line, MAXlen)) != NULL)
+	while((length = updateline(line, MAXLength)) != 0)
 	{
-		length = lineLen(line);
-		if (line[length - 1] == '\n') {
+		//length = lineLen(line);
+		/*if (line[length - 1] == '\n') {
             line[length - 1] = '\0';
             length--;
-        }
+        }*/
 		if (length >= MAXlen)
 			MAXlen = length;
 		copy(line, longest_line);
@@ -27,10 +27,19 @@ int main()
 	return 0;
 }
 
-int getline(char line[], int len)
+int updateline(char line[], int len)
 {
-	
-	return 0;
+	// line[0] = '\0';  
+	int c = ' ';
+	int counter = 0;
+	while ((c = getchar()) != EOF)
+	{
+		printf("Char # %d: %c\n", counter,c );
+		if (counter == len) return counter;
+		line[counter++] = c;
+	}
+	printf("Line : %s\nCounter: %d\n", line, counter);
+	return counter;
 }
 
 int lineLen(char line[])
@@ -48,6 +57,7 @@ int lineLen(char line[])
 
 int copy(char line[], char longest_line[])
 {
+	// longest_line[0] = '\0';
 	int counter = 0;
 	while((longest_line[counter] = line[counter]) != '\0')
 		counter++;

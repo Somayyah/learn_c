ec90d2a980598d5109bf7e0503b13ef59a60c164
watari <watari@techwebunraveled.xyz>
2025-03-25 11:32:52 +0300

    removed unnecesarry function

--- c/char-arr.c
+++ c/char-arr.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int updateline(char line[], int len);
-int lineLen(char line[]);
 int copy(char line[], char longest_line[]);
 int clearLine(char line[]);
 
@@ -15,16 +14,10 @@ int main()
 	int MAXlen = 0;
 	while((length = updateline(line, MAXLength)) != 0)
 	{
-		//length = lineLen(line);
-		/*if (line[length - 1] == '\n') {
-            line[length - 1] = '\0';
-            length--;
-        }*/
 		if (length >= MAXlen){
 			MAXlen = length;
 			copy(line, longest_line);
 		}
-		printf("Line: %s\nLongest_line: %s\nMaxLen: %d\nLength: %d\n", line, longest_line, MAXlen, length);
 	}
 	printf("\nLongest Line: %s\nLength: %d\n", longest_line, MAXlen);
 	return 0;
@@ -37,11 +30,9 @@ int updateline(char line[], int len)
 	int counter = 0;
 	while (((c = getchar()) != EOF) && c != '\n')
 	{
-		// printf("Char # %d: %c\n", counter,c );
 		if (counter == len) return counter;
 		line[counter++] = c;
 	}
-	// printf("Line : %s\nCounter: %d\n", line, counter);
 	return counter;
 }
 
@@ -54,19 +45,6 @@ int clearLine(char line[])
 		counter++;
 	}
 	return 0;
-}
-
-int lineLen(char line[])
-{
-	int counter = 0;
-	int len = 0;
-	while (line[counter] != '\0')
-	{
-		len++;
-		counter++;
-	}
-	return len;
-
 }
 
 int copy(char line[], char longest_line[])

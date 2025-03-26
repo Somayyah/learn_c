9f99911e15fe97cf921741ce1d2e8a873df45322
watari <watari@techwebunraveled.xyz>
2025-03-26 21:54:52 +0300

    removed unnecesarry function

---
+++ c/1-20.c
@@ -0,0 +1,19 @@
+/*
+Exercise 1-20. Write a program detab that replaces tabs in the input with the proper number 
+of blanks to space to the next tab stop. Assume a fixed set of tab stops, 
+say every n columns. Should n be a variable or a symbolic parameter?
+*/
+
+#include <stdio.h>
+
+int detab();
+
+int main()
+{
+	return 0;
+}
+
+int detab()
+{
+	return 0;
+}

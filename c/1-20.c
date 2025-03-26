c731d19af5658b875a81966405c10cf15269e989
watari <watari@techwebunraveled.xyz>
2025-03-26 21:58:34 +0300

    removed unnecesarry function

--- c/1-20.c
+++ c/1-20.c
@@ -7,9 +7,37 @@ say every n columns. Should n be a variable or a symbolic parameter?
 #include <stdio.h>
 
 int detab();
+int updateline(char line[], int len);
+int clearLine(char line[]);
 
 int main()
 {
+	return 0;
+}
+
+
+int updateline(char line[], int len)
+{
+	clearLine(line);
+	int c , counter;
+	c = counter = 0;
+	while (((c = getchar()) != EOF) && c != '\n')
+	{
+		if (counter == len) return counter;
+		line[counter++] = c;
+	}
+	return counter;
+}
+
+
+int clearLine(char line[])
+{
+	int counter = 0;
+	while(line[counter] != '\0')
+	{
+		line[counter] = '\0';
+		counter++;
+	}
 	return 0;
 }
 

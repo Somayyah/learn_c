b5ac5bb37dbfa165b095d0df1763e0f1805c7899
watari <watari@techwebunraveled.xyz>
2025-06-01 15:43:25 +0300

    fold

--- c/1-22.c
+++ c/1-22.c
@@ -7,7 +7,73 @@ before the specified column.
 
 #include <stdio.h>
 
+#define		COLUMN		8
+#define		MAXLen		1000
+
+int fold(char line[], char tempString[]);
+int fillLine(char line[], int len);
+int clearLine(char line[]);
+
 int main()
 {
+	char tempString[MAXLen] = "";
+	char line[MAXLen] = "";
+	int length = 0;
+	int entabLine = 0;
+	while((length = fillLine(line, MAXLen)) != 0)
+	{
+		if ((entabLine = fold(line, tempString)) == 0)
+			printf("%s\n", tempString);
+		clearLine(tempString);
+	}
+	return 0;
+}
+
+
+int fillLine(char line[], int len)
+{
+	clearLine(line);
+	int c , counter;
+	c = counter = 0;
+	while (((c = getchar()) != EOF) && c != '\n')
+	{
+		if (counter == len) return counter;
+		line[counter++] = c;
+	}
+	return counter;
+}
+
+
+int clearLine(char line[])
+{
+	int counter = 0;
+	while(line[counter] != '\0')
+	{
+		line[counter] = '\0';
+		counter++;
+	}
+	return 0;
+}
+
+int fold(char line[], char tempString[])
+{
+	int line_counter = 0;
+	int temp_counter = 0;
+	int edge = 1;
+	while(line[line_counter] != '\0')
+	{
+		if(line_counter != 0) 
+			edge = line_counter % COLUMN;
+		if ( edge != 0 )
+			tempString[temp_counter++] = line[line_counter++];
+		else
+		{
+			if (tempString[temp_counter - 1] != '\t' && tempString[temp_counter] != ' ')
+				tempString[temp_counter++] = '-';
+			tempString[temp_counter++] = '\n';
+			line_counter++;
+		}
+	}
+	tempString[temp_counter] = '\0';
 	return 0;
 }

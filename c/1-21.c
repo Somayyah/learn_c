21873fffe7b814930379e6b8e2bb69d0a8d1e68a
watari <watari@techwebunraveled.xyz>
2025-06-01 14:43:30 +0300

    entab

--- c/1-21.c
+++ c/1-21.c
@@ -6,7 +6,83 @@ either a tab or a single blank would suffice to reach a tab stop, which should b
 
 #include <stdio.h>
 
+#define		TABSTOP		8
+#define		MAXLen		100
+
+int entab(char line[], char tempString[]);
+int fillLine(char line[], int len);
+int clearLine(char line[]);
+
 int main()
 {
+	char tempString[MAXLen] = "";
+	char line[MAXLen] = "Hello         World!";
+	int length = 0;
+	int entabLine = 0;
+	/*while((length = fillLine(line, MAXLen)) != 0)
+	{
+		if ((entabLine = entab(line, tempString)) == 0)
+			printf("%s\n", tempString);
+		clearLine(tempString);
+	}*/
+	entabLine = entab(line, tempString);
+	printf("%s\n",tempString);
+	return 0;
+}
+
+
+int fillLine(char line[], int len)
+{
+	clearLine(line);
+	int c , counter;
+	c = counter = 0;
+	while (((c = getchar()) != EOF) && c != '\n')
+	{
+		if (counter == len) return counter;
+		line[counter++] = c;
+	}
+	return counter;
+}
+
+
+int clearLine(char line[])
+{
+	int counter = 0;
+	while(line[counter] != '\0')
+	{
+		line[counter] = '\0';
+		counter++;
+	}
+	return 0;
+}
+
+int entab(char line[], char tempString[])
+{
+	int line_counter = 0;
+	int temp_counter = 0;
+	int spaces = 0;
+	int tabs = 0;
+	while(line[line_counter] != '\0')
+	{
+		if (line[line_counter] != ' ' )
+		{
+			tempString[temp_counter++] = line[line_counter++];
+		}
+		else
+		{
+			while(line[line_counter++] == ' ')
+				spaces++;
+			tabs = spaces / TABSTOP;
+			spaces = spaces -  (tabs * TABSTOP);
+			for(; tabs > 0; tabs--)
+				tempString[temp_counter++] = '*';
+			for(; spaces > 0; spaces--)
+				tempString[temp_counter++] = '^';
+			//line_counter++;
+			spaces = tabs = 0;
+		}
+	}
+	
+	tempString[temp_counter] = '\0';
 	return 0;
 }

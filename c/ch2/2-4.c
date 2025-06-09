c8ef526e69e9784af904c3d5ac00753ac6c4f11f
watari <watari@techwebunraveled.xyz>
2025-06-09 23:57:58 +0300

    squeeze function

--- c/ch2/2-4.c
+++ c/ch2/2-4.c
@@ -3,11 +3,15 @@ Exercise 2-4. Write an alternative version of squeeze(s1,s2) that deletes each c
 s1 that matches any character in the string s2.
 */
 
+void squeeze(char s[], int c);
+int mysqueeze(char s[], int c, char new_string[]);
+
 int main()
 {
 	char s[] = "";
+	char new_string[] = "";
 	int c = '';
-	printf("The new string : %s\n", mysqueeze(s, c));
+	printf("The new string : %s\n", mysqueeze(s, c, new_string));
 	return 0;
 }
 
@@ -21,7 +25,7 @@ void squeeze(char s[], int c)
 	s[j] = '\0';
 }
 
-int mysqueeze(char s[], int c)
+int mysqueeze(char s[], int c, char new_string[])
 {
 	int i = 0;
 	while (s[i] != '\0')

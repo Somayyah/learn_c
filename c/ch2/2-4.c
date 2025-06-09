0e2b54ec44e79d757c66174a15ec8081c98db81b
watari <watari@techwebunraveled.xyz>
2025-06-10 00:02:14 +0300

    use cases

--- c/ch2/2-4.c
+++ c/ch2/2-4.c
@@ -4,12 +4,12 @@ s1 that matches any character in the string s2.
 */
 
 void squeeze(char s[], int c);
-int mysqueeze(char s[], int c, char new_string[]);
+int mysqueeze(char s[], char s2[]);
 
 int main()
 {
 	char s[] = "";
-	char new_string[] = "";
+	char s2[] = "";
 	int c = '';
 	printf("The new string : %s\n", mysqueeze(s, c, new_string));
 	return 0;
@@ -25,7 +25,7 @@ void squeeze(char s[], int c)
 	s[j] = '\0';
 }
 
-int mysqueeze(char s[], int c, char new_string[])
+int mysqueeze(char s[], char s2[])
 {
 	int i = 0;
 	while (s[i] != '\0')
@@ -35,3 +35,20 @@ int mysqueeze(char s[], int c, char new_string[])
 	
 	return 0;
 }
+
+/*
+| Test Case # | `s1` (Input String)      | `s2` (Characters to Delete) | Expected Result                                  | Notes                       |
+| ----------- | ------------------------ | --------------------------- | ------------------------------------------------ | --------------------------- |
+| 1           | `"hello world"`          | `"aeiou"`                   | `"hll wrld"`                                     | Remove all vowels           |
+| 2           | `"banana"`               | `"n"`                       | `"baaa"`                                         | Repeated chars removed      |
+| 3           | `"abcdefg"`              | `"xyz"`                     | `"abcdefg"`                                      | No matches                  |
+| 4           | `"the quick brown fox"`  | `" "`                       | `"thequickbrownfox"`                             | Remove all spaces           |
+| 5           | `"1234567890"`           | `"2468"`                    | `"135790"`                                       | Digits removal              |
+| 6           | `"AaBbCc"`               | `"abc"`                     | `"AABBC"` (or `"AABBCC"` in case-sensitive test) | Case sensitivity test       |
+| 7           | `"repeat repeat repeat"` | `"re"`                      | `"pat pat pat"`                                  | Multiple matches, scattered |
+| 8           | `""` (empty string)      | `"abc"`                     | `""`                                             | Empty `s1`                  |
+| 9           | `"nothing"`              | `""` (empty `s2`)           | `"nothing"`                                      | Empty `s2`, no removals     |
+| 10          | `""`                     | `""`                        | `""`                                             | Both empty                  |
+| 11          | `"aaaaa"`                | `"a"`                       | `""`                                             | Complete removal            |
+| 12          | `"abcABC"`               | `"ABC"`                     | `"abc"`                                          | Uppercase deletion only     |
+*/

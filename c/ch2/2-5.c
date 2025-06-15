bbe778e7b8f0cdc62426eff7e9a24b019a5cd2be
watari <watari@techwebunraveled.xyz>
2025-06-15 19:37:15 +0300

    2-5.c

--- c/ch2/2-5.c
+++ c/ch2/2-5.c
@@ -13,19 +13,22 @@ int main()
 {
 	char s1[] = "770Hello, world!\n";
 	char s2[] = "10";
-	printf("Position is : %d\n", any(s1, s2));
+	printf("Position is : %d\n", any(s1, NULL));
 	return 0;
 }
 
 int any(char s1[], char s2[])
 {
-	int i;
-	unsigned char exists[STANDARD_NUMBER_OF_CHARS] = {0};
-	
-	for ( i = 0; s2[i] != '\0';i++)
-		exists[(unsigned char)s2[i]] = 1;
-	
-	for (i = 0; s1[i] != '\0'; i++)
-		if (exists[(unsigned char)s1[i]]) return (i);
+	if (!(s2 == NULL || s1 == NULL))
+	{
+		int i;
+		unsigned char exists[STANDARD_NUMBER_OF_CHARS] = {0};
+		
+		for ( i = 0; s2[i] != '\0';i++)
+			exists[(unsigned char)s2[i]] = 1;
+		
+		for (i = 0; s1[i] != '\0'; i++)
+			if (exists[(unsigned char)s1[i]]) return (i);
+	}
 	return -1;
 }

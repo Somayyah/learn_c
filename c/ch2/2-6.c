e32d26179670adab23260cc5c0de8e3645fe0afc
watari <watari@techwebunraveled.xyz>
2025-06-18 23:32:54 +0300

    2-5

--- c/ch2/2-6.c
+++ c/ch2/2-6.c
@@ -1,15 +1,20 @@
 /*
 Exercise 2-6. Write a function setbits(x,p,n,y) that returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged.
+
+x = 0000 1111
+p = 3
+n = 5
+y = 1101 1010
 */
 
-int setbits(int x,int p,int n,int y);
+unsigned setbits(unsigned x,int p,int n,unsigned y);
 
 int main()
 {
 	return 0;
 }
 
-int setbits()
+unsigned setbits(unsigned x,int p,int n,unsigned y)
 {
-	return 0; 
+	return x; 
 }

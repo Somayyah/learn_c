2ea7d9f408a862be1df0c32fcaea1a14c1e750a4
watari <watari@techwebunraveled.xyz>
2025-08-25 12:51:30 +0300

    1-10

---
+++ c/ch1/1-10.c
@@ -0,0 +1,28 @@
+/*
+	Exercise 1-10. Write a program to copy its input to its output, replacing each tab by \t, each backspace by \b, and each backslash by \\. This makes tabs and backspaces visible in an unambiguous way.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int c = 0;
+	while ((c = getchar()) != EOF)
+	{
+		if (c == '\t')
+		{
+			putchar('\\');
+			putchar('t');
+		} else if (c == '\\')
+		{
+			putchar('\\');
+			putchar('\\');
+		} else if (c == '\b')
+		{
+			putchar('\\');
+			putchar('b');
+		}
+		else putchar(c);
+	}
+	return 0;
+}

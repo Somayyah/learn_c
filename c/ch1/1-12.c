8a13f0f4dfcb6ae8ecc08dd52ad8a6e6a1b0e07e
watari <watari@techwebunraveled.xyz>
2025-08-27 10:28:51 +0300

    1-12

--- c/ch1/1-12.c
+++ c/ch1/1-12.c
@@ -1,3 +1,39 @@
 /*
 	Exercise 1-12. Write a program that prints its input one word per line.
 */
+
+
+#include <stdio.h>
+
+// Are we within the boundary of multiple whitespace characters?
+#define IN	1  
+#define OUT 0
+
+int main()
+{
+	int c, state, flag;
+	c = flag = 0;
+	state = OUT;
+	
+	while ((c = getchar()) != EOF) 
+	{
+		if (c == ' ' || c == '\n' || c == '\t' )
+		{
+			if (state == OUT && flag != 0 ) putchar('\n');
+			state = IN;
+		}
+		
+		else 
+		{
+			flag = 1;
+			putchar(c);
+			state = OUT;
+		}
+	}
+	
+	if (state == OUT && flag ) {
+		putchar('\n');
+	}
+
+	return 0;
+}

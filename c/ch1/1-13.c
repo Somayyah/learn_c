ca49bfd931fcf642249b8ae1d840e817d459a206
watari <watari@techwebunraveled.xyz>
2025-08-29 13:07:38 +0300

    1-13

--- c/ch1/1-13.c
+++ c/ch1/1-13.c
@@ -6,10 +6,16 @@
  
 #include <stdio.h>
 
+#define		IN							1 // Inside a word's boundary
+#defile 	OUT 						0 // Outside a word's boundary
+#define 	MAX_NUMBER_OF_LETTERS		20
 int main()
 {
-	int c;
-	c = 0;
+	int c, c_count[MAX_NUMBER_OF_LETTERS], nc;
+	c = nc = 0;
+	
+	for (i = 0; i < MAX_NUMBER_OF_LETTERS; ++i)
+		c_count[i] = 0;
 	
 	while((c = getchar()) != EOF) 
 	{

3be1e69d19a19ff5363f01a346c7f463b1d81961
watari <watari@techwebunraveled.xyz>
2025-09-10 13:51:36 +0300

    1-13

--- c/ch1/1-13.c
+++ c/ch1/1-13.c
@@ -6,21 +6,42 @@
  
 #include <stdio.h>
 
-#define		IN							1 // Inside a word's boundary
-#defile 	OUT 						0 // Outside a word's boundary
-#define 	MAX_NUMBER_OF_LETTERS		20
+#define 	MAX_NUMBER_OF_LETTERS		21
+
 int main()
 {
 	int c, c_count[MAX_NUMBER_OF_LETTERS], nc;
 	c = nc = 0;
 	
-	for (i = 0; i < MAX_NUMBER_OF_LETTERS; ++i)
+	for (int i = 0; i < MAX_NUMBER_OF_LETTERS; ++i)
 		c_count[i] = 0;
 	
 	while((c = getchar()) != EOF) 
 	{
-		
+		if ( c != '\n' && c != ' ' && c != '\t' )
+			nc++;
+		else 
+		{
+			if (nc < MAX_NUMBER_OF_LETTERS - 1)
+				++c_count[nc];
+			else c_count[MAX_NUMBER_OF_LETTERS - 1]++; // Any word longer than 20 is counted here.
+			nc = 0;
+		}
 	}
 
+	if ( nc != 0 )
+		++c_count[nc];
+	
+	putchar('\n');
+	
+	for(int i = 1; i < MAX_NUMBER_OF_LETTERS; i++)
+		if (c_count[i] != 0)
+		{
+			printf("%d : ", i);
+			for (int j = 0; j < c_count[i] ; j++ )
+				putchar('*');
+			putchar('\n');
+		}
+	
 	return 0;
 }
